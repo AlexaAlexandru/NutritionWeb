d8f102f9d81373981df9faa9ee68aec0a03afa2a
AlexaAlexandru <liv06ale@gmail.com>
2023-11-18 16:11:14 +0200

    feat: CLEAN architecture

---
+++ Application/AutoFacModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Application.Behaviours;
+using Autofac;
+using MediatR;
+using MediatR.Extensions.Autofac.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection.Builder;
+
+namespace Application
+{
+    public sealed class AutoFacModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterMediatR(MediatRConfigurationBuilder.Create(ThisAssembly).WithAllOpenGenericHandlerTypesRegistered().Build());
+
+            builder.RegisterGeneric(typeof(ValidationBehaviour<,>)).As(typeof(IPipelineBehavior<,>)).InstancePerLifetimeScope();
+
+        }
+    }
+}
+
